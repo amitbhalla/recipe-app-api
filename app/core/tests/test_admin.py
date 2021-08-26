92f16d462ba4fe651a0d070a6e2b856a803502b1
amitbhalla <amit9815@gmail.com>
2021-08-26 10:56:37 +0530

    Added admin portal tests

---
+++ app/core/tests/test_admin.py
@@ -0,0 +1,33 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+
+class AdminSiteTests(TestCase):
+    def setUp(self):
+        """Setup by creating a user and a superuser"""
+        self.client = Client()
+        superuser_email = "superuser@mail.com"
+        superuser_password = "TestPass123"
+        superuser_name = "Test Super User"
+        self.admin_user = get_user_model().objects.create_superuser(
+            email=superuser_email,
+            password=superuser_password,
+            name=superuser_name,
+        )
+        user_email = "user@mail.com"
+        user_password = "TestPass123"
+        user_name = "Test User"
+        self.client.force_login(self.admin_user)
+        self.user = get_user_model().objects.create_user(
+            email=user_email, password=user_password, name=user_name
+        )
+
+    def test_users_listed(self):
+        """Test that users are listed on the admin portal user page"""
+        url = reverse("admin:core_user_changelist")
+        response = self.client.get(url)
+
+        self.assertContains(response, self.user.name)
+        self.assertContains(response, self.user.email)
