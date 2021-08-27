fa99b4f47e12bb3e74c9373717ff431035211697
amitbhalla <amit9815@gmail.com>
2021-08-27 09:16:48 +0530

    Tags model created

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -1,6 +1,18 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from core import models
+
+
+def sample_user():
+    """Creates a sample user for our tests"""
+    email = "test@mail.com"
+    password = "TestPass123"
+    name = "Some Name"
+    return get_user_model().objects.create_user(
+        email=email, password=password, name=name
+    )
+
 
 class ModelTests(TestCase):
     def test_create_user_with_email_successful(self):
@@ -10,6 +22,7 @@ class ModelTests(TestCase):
         user = get_user_model().objects.create_user(
             email=email, password=password
         )
+
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
 
@@ -20,6 +33,7 @@ class ModelTests(TestCase):
         user = get_user_model().objects.create_user(
             email=email, password=password
         )
+
         self.assertEqual(user.email, email.lower())
 
     def test_new_user_invalid_email(self):
@@ -35,5 +49,12 @@ class ModelTests(TestCase):
         user = get_user_model().objects.create_superuser(
             email=email, password=password
         )
+
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_tag_str(self):
+        """Test that tag string repersentation"""
+        tag = models.Tag.objects.create(user=sample_user(), name="Vegan")
+
+        self.assertEqual(str(tag), tag.name)
